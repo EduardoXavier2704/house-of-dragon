cd941cf07dff02ad392f1e1e6236f793f2862c48
Eduardo.Xavier <eduardobx1234@gmail.com>
2024-08-11 01:04:18 -0300

    Finalizado

---
+++ src/js/index.js
@@ -0,0 +1,48 @@
+const botoesCarrosel = document.querySelectorAll(".botao");
+const imagens = document.querySelectorAll(".imagem");
+const informacoes = document.querySelectorAll(".informacoes");
+
+botoesCarrosel.forEach((botao, indice) => {
+    botao.addEventListener('click', () => {
+
+        desativarBotaoSelecionado();
+
+        marcarBotaoSelecionado(botao);
+
+        esconderImagemAtiva();
+
+        mostrarImagemDeFundo(indice);
+
+        esconderInformacoesAtivas();
+
+        mostrarInformacoes(indice);
+    });
+});
+
+function marcarBotaoSelecionado(botao) {
+    botao.classList.add("selecionado");
+}
+
+function mostrarInformacoes(indice) {
+    informacoes[indice].classList.add("ativa");
+}
+
+function esconderInformacoesAtivas() {
+    const informacoesAtiva = document.querySelector(".informacoes.ativa");
+    informacoesAtiva.classList.remove("ativa");
+}
+
+function mostrarImagemDeFundo(indice) {
+    imagens[indice].classList.add("ativa");
+}
+
+function esconderImagemAtiva() {
+    const imagemAtiva = document.querySelector(".ativa");
+    imagemAtiva.classList.remove("ativa");
+}
+
+function desativarBotaoSelecionado() {
+    const botaoSelecionado = document.querySelector(".selecionado");
+    botaoSelecionado.classList.remove("selecionado");
+}
+
